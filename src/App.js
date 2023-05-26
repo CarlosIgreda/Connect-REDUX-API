d9455c4b2c5dad9a2f9bdecac8aa74c80c3380b5
CarlosIgreda <cjigredarivas@gmail.com>
2023-05-26 02:15:03 -0500

    Set up linters

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.css';
+import UsersList from './components/UsersList';
+
+function App() {
+  return (
+    <div className="App">
+      <UsersList />
+    </div>
+  );
+}
+
+export default App;
